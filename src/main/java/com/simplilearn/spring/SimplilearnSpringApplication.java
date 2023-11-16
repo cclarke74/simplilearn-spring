19b48f36880beba07f4f2150b941598656f861be
cclarke74 <clairc2@gmail.com>
2023-11-16 14:37:10 -0800

    first commit

---
+++ src/main/java/com/simplilearn/spring/SimplilearnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnSpringApplication.class, args);
+	}
+
+}
