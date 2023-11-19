19eb3be4e7441bc54cac2a2e641c7de3529c0c92
cclarke74 <clairc2@gmail.com>
2023-11-19 13:10:47 -0800

    Add Dependency Injection

---
+++ src/main/java/com/simplilearn/spring/dependency_injection/_01_without_di/WithoutDIExample.java
@@ -0,0 +1,54 @@
+package com.simplilearn.spring.dependency_injection._01_without_di;
+
+
+class Invoice{
+
+    void print() {
+        System.out.println("Printing the Invoice");
+    }
+}
+
+class Quote{
+
+    void print() {
+        System.out.println("Printing the Quote");
+    }
+}
+
+class Order{
+
+    void print() {
+        System.out.println("Printing the Order");
+    }
+}
+
+class Printer{
+
+    /*
+     * The class printer depends on Invoice/Quote/Order
+     */
+
+    Invoice invoice = new Invoice(); // High/Tight Coupling
+
+    Quote quote = new Quote();
+
+    Order order = new Order();
+
+    void printInvoice() {
+        this.invoice.print();
+    }
+
+    void printQuote() {
+        this.quote.print();
+    }
+
+    void printOrder() {
+        this.order.print();
+    }
+
+}
+
+public class WithoutDIExample {
+
+}
+
